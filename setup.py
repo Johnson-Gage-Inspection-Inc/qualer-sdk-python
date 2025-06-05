1cfa6a57dc37c1e58f2ccdaeb1b130c3aa8a3a14
Jeffrey Hall <75549255+rhythmatician@users.noreply.github.com>
2025-06-04 23:48:39 -0500

    Refactor setup.py for clarity and organization (#4)
    This PR adds a py.typed marker to the package manifest, aligning with a refactored packaging strategy for improved clarity and organization, and removes numerous auto‐generated API modules from the build library .
    
    + Added an inclusion for the type check file in MANIFEST.in.
    + Removed old Swagger-generated API files from the build/lib/qualer_sdk/api directory.
    
    Fixes #3

--- setup.py
+++ setup.py
@@ -1,17 +1,16 @@
 from setuptools import find_packages, setup
 
 setup(
-    name="qualer-sdk",
-    version="2.2.1",
-    description="Auto-generated Python client for Qualer",
     author="Johnson Gage & Inspection Inc.",
     author_email="jhall@jgiquality.com",
-    packages=find_packages(where="qualer_sdk"),
-    package_dir={"": "qualer_sdk"},
-    install_requires=["urllib3>=1.15", "six>=1.10", "certifi", "python-dateutil"],
-    python_requires=">=3.7",
+    description="Auto-generated Python client for Qualer",
     include_package_data=True,
+    install_requires=["urllib3>=1.15", "six>=1.10", "certifi", "python-dateutil"],
+    name="qualer-sdk",
     package_data={"qualer_sdk": ["py.typed"]},
+    packages=find_packages(),
+    python_requires=">=3.7",
+    version="2.2.1",
     classifiers=[
         "Programming Language :: Python :: 3",
         "Operating System :: OS Independent",
