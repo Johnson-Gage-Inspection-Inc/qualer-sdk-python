62ed58db12a13651519520cbb292a0301a87390a
Jeff Hall <rhythmatician5@gmail.com>
2025-06-04 22:54:16 -0500

    Refactor setup.py for improved clarity and organization

--- setup.py
+++ setup.py
@@ -1,17 +1,17 @@
 from setuptools import find_packages, setup
 
 setup(
-    name="qualer-sdk",
-    version="2.2.1",
-    description="Auto-generated Python client for Qualer",
     author="Johnson Gage & Inspection Inc.",
     author_email="jhall@jgiquality.com",
-    packages=find_packages(where="qualer_sdk"),
-    package_dir={"": "qualer_sdk"},
-    install_requires=["urllib3>=1.15", "six>=1.10", "certifi", "python-dateutil"],
-    python_requires=">=3.7",
+    description="Auto-generated Python client for Qualer",
     include_package_data=True,
-    package_data={"qualer_sdk": ["py.typed"]},
+    install_requires=["urllib3>=1.15", "six>=1.10", "certifi", "python-dateutil"],
+    name="qualer-sdk",
+    package_data={"": ["py.typed"]},
+    package_dir={"": "qualer_sdk"},
+    packages=find_packages(where="qualer_sdk"),
+    python_requires=">=3.7",
+    version="2.2.1",
     classifiers=[
         "Programming Language :: Python :: 3",
         "Operating System :: OS Independent",
