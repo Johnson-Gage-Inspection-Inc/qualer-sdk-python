9d17791b01aee8b26d7ebcdf94e95f0c070c6991
Jeff Hall <rhythmatician5@gmail.com>
2025-06-17 23:13:08 -0500

    fix: update models to allow nullable date fields and improve parsing logic

--- src/qualer_sdk/models/qualer_api_models_asset_service_records_to_asset_service_record_response_model.py
+++ src/qualer_sdk/models/qualer_api_models_asset_service_records_to_asset_service_record_response_model.py
@@ -1,6 +1,6 @@
 import datetime
 from collections.abc import Mapping
-from typing import Any, TypeVar, Union
+from typing import Any, TypeVar, Union, cast
 from uuid import UUID
 
 from attrs import define as _attrs_define
@@ -30,7 +30,7 @@ class QualerApiModelsAssetServiceRecordsToAssetServiceRecordResponseModel:
         result_status (Union[Unset, str]):
         as_found_result (Union[Unset, str]):
         as_left_result (Union[Unset, str]):
-        service_date (Union[Unset, datetime.datetime]):
+        service_date (Union[None, Unset, datetime.datetime]):
         serial_number (Union[Unset, str]):
         asset_tag (Union[Unset, str]):
         asset_user (Union[Unset, str]):
@@ -38,8 +38,8 @@ class QualerApiModelsAssetServiceRecordsToAssetServiceRecordResponseModel:
         asset_user_change (Union[Unset, str]):
         service_notes (Union[Unset, str]):
         serial_number_change (Union[Unset, str]):
-        due_date (Union[Unset, datetime.datetime]):
-        next_service_date (Union[Unset, datetime.datetime]):
+        due_date (Union[None, Unset, datetime.datetime]):
+        next_service_date (Union[None, Unset, datetime.datetime]):
         provider_technician (Union[Unset, str]):
         provider_phone (Union[Unset, str]):
         provider_company (Union[Unset, str]):
@@ -70,7 +70,7 @@ class QualerApiModelsAssetServiceRecordsToAssetServiceRecordResponseModel:
     result_status: Union[Unset, str] = UNSET
     as_found_result: Union[Unset, str] = UNSET
     as_left_result: Union[Unset, str] = UNSET
-    service_date: Union[Unset, datetime.datetime] = UNSET
+    service_date: Union[None, Unset, datetime.datetime] = UNSET
     serial_number: Union[Unset, str] = UNSET
     asset_tag: Union[Unset, str] = UNSET
     asset_user: Union[Unset, str] = UNSET
@@ -78,8 +78,8 @@ class QualerApiModelsAssetServiceRecordsToAssetServiceRecordResponseModel:
     asset_user_change: Union[Unset, str] = UNSET
     service_notes: Union[Unset, str] = UNSET
     serial_number_change: Union[Unset, str] = UNSET
-    due_date: Union[Unset, datetime.datetime] = UNSET
-    next_service_date: Union[Unset, datetime.datetime] = UNSET
+    due_date: Union[None, Unset, datetime.datetime] = UNSET
+    next_service_date: Union[None, Unset, datetime.datetime] = UNSET
     provider_technician: Union[Unset, str] = UNSET
     provider_phone: Union[Unset, str] = UNSET
     provider_company: Union[Unset, str] = UNSET
@@ -123,9 +123,13 @@ class QualerApiModelsAssetServiceRecordsToAssetServiceRecordResponseModel:
 
         as_left_result = self.as_left_result
 
-        service_date: Union[Unset, str] = UNSET
-        if not isinstance(self.service_date, Unset):
+        service_date: Union[None, Unset, str]
+        if isinstance(self.service_date, Unset):
+            service_date = UNSET
+        elif isinstance(self.service_date, datetime.datetime):
             service_date = self.service_date.isoformat()
+        else:
+            service_date = self.service_date
 
         serial_number = self.serial_number
 
@@ -141,13 +145,21 @@ class QualerApiModelsAssetServiceRecordsToAssetServiceRecordResponseModel:
 
         serial_number_change = self.serial_number_change
 
-        due_date: Union[Unset, str] = UNSET
-        if not isinstance(self.due_date, Unset):
+        due_date: Union[None, Unset, str]
+        if isinstance(self.due_date, Unset):
+            due_date = UNSET
+        elif isinstance(self.due_date, datetime.datetime):
             due_date = self.due_date.isoformat()
+        else:
+            due_date = self.due_date
 
-        next_service_date: Union[Unset, str] = UNSET
-        if not isinstance(self.next_service_date, Unset):
+        next_service_date: Union[None, Unset, str]
+        if isinstance(self.next_service_date, Unset):
+            next_service_date = UNSET
+        elif isinstance(self.next_service_date, datetime.datetime):
             next_service_date = self.next_service_date.isoformat()
+        else:
+            next_service_date = self.next_service_date
 
         provider_technician = self.provider_technician
 
@@ -292,12 +304,22 @@ class QualerApiModelsAssetServiceRecordsToAssetServiceRecordResponseModel:
 
         as_left_result = d.pop("AsLeftResult", UNSET)
 
-        _service_date = d.pop("ServiceDate", UNSET)
-        service_date: Union[Unset, datetime.datetime]
-        if isinstance(_service_date, Unset):
-            service_date = UNSET
-        else:
-            service_date = isoparse(_service_date)
+        def _parse_service_date(data: object) -> Union[None, Unset, datetime.datetime]:
+            if data is None:
+                return data
+            if isinstance(data, Unset):
+                return data
+            try:
+                if not isinstance(data, str):
+                    raise TypeError()
+                service_date_type_0 = isoparse(data)
+
+                return service_date_type_0
+            except:  # noqa: E722
+                pass
+            return cast(Union[None, Unset, datetime.datetime], data)
+
+        service_date = _parse_service_date(d.pop("ServiceDate", UNSET))
 
         serial_number = d.pop("SerialNumber", UNSET)
 
@@ -313,19 +335,41 @@ class QualerApiModelsAssetServiceRecordsToAssetServiceRecordResponseModel:
 
         serial_number_change = d.pop("SerialNumberChange", UNSET)
 
-        _due_date = d.pop("DueDate", UNSET)
-        due_date: Union[Unset, datetime.datetime]
-        if isinstance(_due_date, Unset):
-            due_date = UNSET
-        else:
-            due_date = isoparse(_due_date)
+        def _parse_due_date(data: object) -> Union[None, Unset, datetime.datetime]:
+            if data is None:
+                return data
+            if isinstance(data, Unset):
+                return data
+            try:
+                if not isinstance(data, str):
+                    raise TypeError()
+                due_date_type_0 = isoparse(data)
 
-        _next_service_date = d.pop("NextServiceDate", UNSET)
-        next_service_date: Union[Unset, datetime.datetime]
-        if isinstance(_next_service_date, Unset):
-            next_service_date = UNSET
-        else:
-            next_service_date = isoparse(_next_service_date)
+                return due_date_type_0
+            except:  # noqa: E722
+                pass
+            return cast(Union[None, Unset, datetime.datetime], data)
+
+        due_date = _parse_due_date(d.pop("DueDate", UNSET))
+
+        def _parse_next_service_date(
+            data: object,
+        ) -> Union[None, Unset, datetime.datetime]:
+            if data is None:
+                return data
+            if isinstance(data, Unset):
+                return data
+            try:
+                if not isinstance(data, str):
+                    raise TypeError()
+                next_service_date_type_0 = isoparse(data)
+
+                return next_service_date_type_0
+            except:  # noqa: E722
+                pass
+            return cast(Union[None, Unset, datetime.datetime], data)
+
+        next_service_date = _parse_next_service_date(d.pop("NextServiceDate", UNSET))
 
         provider_technician = d.pop("ProviderTechnician", UNSET)
 
