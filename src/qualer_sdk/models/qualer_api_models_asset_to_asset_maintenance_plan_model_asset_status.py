27e8183f21be2be196379f358fb405bd4d3592b2
Jeff Hall <rhythmatician5@gmail.com>
2025-10-01 15:09:35 -0500

    Fix deprecation warning messages for asset and service order response models

--- src/qualer_sdk/models/qualer_api_models_asset_to_asset_maintenance_plan_model_asset_status.py
+++ src/qualer_sdk/models/qualer_api_models_asset_to_asset_maintenance_plan_model_asset_status.py
@@ -5,8 +5,7 @@ from qualer_sdk.models.asset_status import AssetStatus
 AssetToAssetMaintenancePlanModelAssetStatus = AssetStatus
 
 warnings.warn(
-    "AssetToAssetMaintenancePlanModelAssetStatus is deprecated and will be removed in a future release. "
-    "Please use AssetStatus instead.",
+    "AssetToAssetMaintenancePlanModelAssetStatus is deprecated and will be removed in a future release. Please use AssetStatus instead.",
     DeprecationWarning,
     stacklevel=2,
 )
--- src/qualer_sdk/models/qualer_api_models_service_orders_to_provider_service_order_response_model_order_status.py
+++ src/qualer_sdk/models/qualer_api_models_service_orders_to_provider_service_order_response_model_order_status.py
@@ -5,8 +5,7 @@ from qualer_sdk.models.service_order_status import ServiceOrderStatus
 ServiceOrdersToProviderServiceOrderResponseModelOrderStatus = ServiceOrderStatus
 
 warnings.warn(
-    "ServiceOrdersToProviderServiceOrderResponseModelOrderStatus is deprecated and will be removed in a future release. "
-    "Please use ServiceOrderStatus instead.",
+    "ServiceOrdersToProviderServiceOrderResponseModelOrderStatus is deprecated and will be removed in a future release. Please use ServiceOrderStatus instead.",
     DeprecationWarning,
     stacklevel=2,
 )
