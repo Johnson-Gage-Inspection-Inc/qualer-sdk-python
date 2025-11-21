d430207c3a11607300d75eb0d0fc9fae7e0ee232
Jeff Hall <rhythmatician5@gmail.com>
2025-11-20 20:58:42 -0600

    refactor: add _missing_ method to ToolRole for integer to string mapping

--- src/qualer_sdk/models/tool_role.py
+++ src/qualer_sdk/models/tool_role.py
@@ -9,3 +9,17 @@ class ToolRole(str, Enum):
 
     def __str__(self) -> str:
         return str(self.value)
+
+    @classmethod
+    def _missing_(cls, value):
+        """Handle integer to string mapping for API responses."""
+        if isinstance(value, int):
+            _int_to_str_map = {
+                0: "Reference Standard",
+                1: "Environmental Sensor",
+                2: "Controlled Environment",
+                3: "Scientific Instrument",
+            }
+            if value in _int_to_str_map:
+                return cls(_int_to_str_map[value])
+        return super()._missing_(value)
